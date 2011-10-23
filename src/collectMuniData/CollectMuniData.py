ba07c2763145a59e455242c125759e90761292fe
Fred <stake75@hotmail.com>
2011-10-23 15:47:24 -0700

    Added first python file to collect data from the Muni servers. The file is currently empty.

---
+++ src/collectMuniData/CollectMuniData.py
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------------------------
+#
+#   CollectMuniData.py
+#   By: Fred Stakem
+#   Date: 10.23.11
+#
+#   Purpose: This file will connect to the Muni server and download data 
+#            so we some data to play with.
+#
+# -----------------------------------------------------------------------------
+
+
